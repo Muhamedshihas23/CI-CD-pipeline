523d52585d6913981a9f3d09b3b32049d07118e7
Muhamed Shihas <muhamedshihas001.com>
2025-11-18 09:10:18 +0000

    updated files

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello from Flask App!"
-
-@app.route('/health')
-def health():
-    return jsonify(status="ok")
+@app.route("/")
+def home():
+    return "Hello from CI/CD Pipeline!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
